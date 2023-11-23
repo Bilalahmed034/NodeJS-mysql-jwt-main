e046086a2b35ca2f4c31f768be74137f49231616
Bilalahmed034 <Bilalahmed034@gmail.com>
2023-11-23 20:00:25 +0500

    Updated animations upload.js

---
+++ src/middlewares/upload.js
@@ -0,0 +1,26 @@
+const util = require("util");
+const multer = require("multer");
+const maxSize = 2 * 1024 * 1024;
+const path = require('path');
+
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log(__dirname);
+    cb(null,  path.join(__dirname, '../uploads/profile'));
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    let extArray = file.mimetype.split("/");
+    let extension = extArray[extArray.length - 1];
+    req.imageName = file.fieldname + '-' + Date.now()+ '.' +extension
+    cb(null, file.fieldname + '-' + Date.now()+ '.' +extension)
+    
+  },
+});
+
+let uploadFile = multer({
+  storage: storage,
+  limits: { fileSize: maxSize },
+}).single("file");
+
+module.exports = uploadFile;
